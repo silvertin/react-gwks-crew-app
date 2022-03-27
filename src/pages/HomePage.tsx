03a225666d2daf5cf9061de901102939203e7751
Philips413 <kimehfud@gmail.com>
2022-03-28 02:25:22 +0900

    feature: Swiper

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import { Carousel, CarouselCaption, CarouselControl, CarouselIndicators, CarouselItem } from "reactstrap";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination} from 'swiper';
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+
+SwiperCore.use([Navigation, Pagination])
 
 const HomePage = () => {
     return (
         <>
-            홈 화면
+            <Swiper
+                className="banner"
+                spaceBetween={50}
+                slidesPerView={1}
+                loop={true}
+                pagination={{ clickable: true }}
+            >
+                <SwiperSlide>
+                    <img src={"https://i.picsum.photos/id/995/388/450.jpg?hmac=wVsm3ShvQtdZlSwnHlfVLnultsYE5tsN85GNkofpEJU"} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={"https://i.picsum.photos/id/811/388/450.jpg?hmac=xNpNZ2_Rsw15SwXoYDR3UDwgcsqw93HisMCzSFmimqY"} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={"https://i.picsum.photos/id/250/388/450.jpg?hmac=C2e7MWQSfGbqAwjorn5l9EUX-q5L-d4Dx8ly8pasAyQ"} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={"https://picsum.photos/388/450"} />
+                </SwiperSlide>
+            </Swiper>
         </>
     )
 }
